caf58f61f2b87ed88023a136018fff6148a0d4a4
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-22 00:51:47 -0300

    Make database initialization authentication optional

--- Npm/connection/ConnectionProvider.spec.ts
+++ Npm/connection/ConnectionProvider.spec.ts
@@ -1,26 +1,38 @@
 import {ConnectionOptions} from './ConnectionOptions'
 import {ConnectionProvider} from './ConnectionProvider'
 import {SinonStub, stub} from 'sinon'
-import {HubConnection, IRetryPolicy} from '@microsoft/signalr'
+import {HubConnection, HubConnectionBuilder, IRetryPolicy} from '@microsoft/signalr'
 
 describe('ConnectionProvider', () => {
+	let options: ConnectionOptions
+	let hubConnectionBuilder: HubConnectionBuilder
+	let hubConnection: HubConnection
+
 	let connectionProvider: ConnectionProvider
 
-	it('constructs correctly the database with the specified options', () => {
-		// Given some options
-		const options: ConnectionOptions = {
+	beforeEach(() => {
+		options = {
 			url: 'http://localhost:40801',
 			getToken: () => 'some-token'
 		}
 
-		const hubConnection: HubConnection = {} as any
-		const hubConnectionBuilder = {withAutomaticReconnect: stub(), withUrl: stub(), build: stub()}
-		hubConnectionBuilder.withAutomaticReconnect.returns(hubConnectionBuilder)
-		hubConnectionBuilder.withUrl.returns(hubConnectionBuilder)
-		hubConnectionBuilder.build.returns(hubConnection)
+		hubConnection = {} as any
+		const withAutomaticReconnect = stub()
+		const withUrl = stub()
+		const build = stub()
+		hubConnectionBuilder = {
+			withAutomaticReconnect,
+			withUrl,
+			build
+		} as any
+		withAutomaticReconnect.returns({withUrl})
+		withUrl.returns({build})
+		build.returns(hubConnection)
 
 		connectionProvider = new ConnectionProvider(hubConnectionBuilder as any)
+	})
 
+	it('constructs correctly the database with the specified options', () => {
 		// When asked for a new connection
 		const connection = connectionProvider.createConnection(options)
 
@@ -38,6 +50,20 @@ describe('ConnectionProvider', () => {
 
 		stubUnderAsserts = hubConnectionBuilder.build as SinonStub
 		expect(stubUnderAsserts.calledOnce).toBeTruthy()
+
+		expect(connection).toBe(hubConnection)
+	})
+
+	it('constructs correctly the database without authentication token, if getToken options is undefined', () => {
+		// Given options that have no getToken() function
+		options = {...options, getToken: null}
+
+		// When asked for a new connection
+		const connection = connectionProvider.createConnection(options)
+
+		// Then the connection should have reconnection logic
+		let stubUnderAsserts = hubConnectionBuilder.withUrl as SinonStub
+		expect(stubUnderAsserts.calledOnceWithExactly(options.url, {withCredentials: false, headers: {}})).toBeTruthy()
 
 		expect(connection).toBe(hubConnection)
 	})
--- Npm/connection/ConnectionProvider.ts
+++ Npm/connection/ConnectionProvider.ts
@@ -1,5 +1,6 @@
 import {HubConnection, HubConnectionBuilder, IRetryPolicy} from '@microsoft/signalr'
 import {ConnectionOptions} from './ConnectionOptions'
+import {isNil} from 'lodash'
 
 /**
  * An object that can create new database connections given database options.
@@ -22,19 +23,20 @@ export class ConnectionProvider {
 	}
 
 	/**
-	 * Creates a new database connection with given options.
+	 * Creates a new database connection with given options. If the getToken function in the options is specified,
+	 * then an Authorization header is issued to on the initialization request, and it contains as value the result of
+	 * invoking getToken() function.
 	 *
 	 * @param options {Partial<ConnectionOptions>}: Options to use to configure the database connection.
 	 * @returns {HubConnection} A SignalR connection to the SignalR endpoint on the backend through which this client and
 	 * the backend will exchange data change notifications
 	 */
 	createConnection(options: Partial<ConnectionOptions>): HubConnection {
+		const headers = isNil(options.getToken) ? {} : {Authorization: `Bearer ${options.getToken()}`}
+
 		return this._builder
 			.withAutomaticReconnect(this._retryPolicy)
-			.withUrl(options.url, {
-				withCredentials: false,
-				headers: {Authorization: `Bearer ${options.getToken()}`}
-			})
+			.withUrl(options.url, {withCredentials: false, headers})
 			.build()
 	}
 }
