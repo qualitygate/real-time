2804672143b85d15fb22dddf3e0cd0bc7f463b5b
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-25 16:44:26 -0300

    Add Matches operator on npm-package

--- Npm/src/contracts/queries/Operator.ts
+++ Npm/src/contracts/queries/Operator.ts
@@ -1,7 +1,8 @@
 export const Equal = '='
 export const NotEqual = '<>'
+export const Matches = 'matches'
 
 /**
  * Defines the type of operators supported in Queries Where conditions.
  */
-export type Operator = typeof Equal | typeof NotEqual
+export type Operator = typeof Equal | typeof NotEqual | typeof Matches
